02c9f465100eebca208d92f268de454f665cdf01
V.Bondaruk <slaff@pow.la>
2020-10-27 22:02:13 -0300

    wip on devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,10 @@ class ApplicationController < ActionController::Base
   layout :layout_by_resource
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
-  
-  # def after_sign_in_path_for(resource)
-  #   if current_user.admin?
-  #     redirect_to backoffice_root_path, notice: "Logeaste a Backoffice"
-  #   else
-  #     redirect_to root_path, notice: "Bienvenido #{current_user.full_name}"
-  #   end
-  # end
+
+  def after_sign_in_path_for(resource)
+    backoffice_root_path
+  end
     
   def layout_by_resource
     if devise_controller?
@@ -25,5 +20,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end  
 end
